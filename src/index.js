5df8ae7dcc360154c61bd194e66718703516fb9c
Matthew Salmon <matt@lab82.com>
2017-05-08 11:27:50 +1000

    Update index.js
    Handle the app going into the background by resetting the listener on next load.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React, { Component, PropTypes } from "react"
-import { BackAndroid, Platform } from "react-native"
+import { AppState, BackAndroid, Platform } from "react-native"
 import withSideEffect from "react-side-effect"
 
-let listener = null
-let backButtonPressFunction = () => false
+var listener = null
+var backButtonPressFunction = () => false
 
 class AndroidBackButton extends Component {
-
   componentDidMount() {
+    if (Platform.OS === "android") {
+      AppState.addEventListener('change', state => {
+        if (state == 'background') {
+          listener = null;
+        }
+      })
+    }
+
     if (Platform.OS === "android" && listener === null) {
       listener = BackAndroid.addEventListener("hardwareBackPress", () => {
         return backButtonPressFunction()
-      })
+      })      
+    }
+  }
+
+  componentWillUnmount() {
+    if (Platform.OS === "android" && listener !== null) {
+      BackAndroid.removeEventListener('hardwareBackPress', listener);
     }
   }
 
